f1046c83102435489f873f140179b80cd8468668
StefanLaux <stefan.laux@edu.tbz.ch>
2022-12-21 20:49:21 +0100

    better colors

--- src/Content/Pages/Home.tsx
+++ src/Content/Pages/Home.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import "../../CSS/Home.css"
-
+import { useInView } from 'framer-motion';
+import { useEffect } from 'react';
+import { animate } from 'motion';
 
 
 
 export default function Home() {
 
+  const refTitle = React.useRef(null);
+  const isInViewTitle = useInView(refTitle);
+
+  
+
+useEffect(() => {
+    if (isInViewTitle) {
+      
+    }
+  }, [isInViewTitle]);
    
     return (
         <div className='wrap' >
@@ -16,15 +28,27 @@ export default function Home() {
             <p>Stefan Laux</p>
           </div>
           <div className="nav">
-          <p className='leftBtn'>&#60;Work /&#x3e; </p><p>|</p><p className='rightBtn'> &#60;About /&#x3e;</p>
+           
+            <p className='leftBtn'>&#60;Work /&#x3e; </p>
+            <p>|</p>
+            <p className='rightBtn'> &#60;About /&#x3e;</p>
+      
           </div>
 
         </div>
 
 
-         <div className='professionName'>
-            <h1 className='underline'>CREATIVE</h1><h1 className='highlight'>DEVELOPER</h1>
-         </div>
+        <h1 className='m19'>
+          <span className='text-wrapper'>
+            <span className='letters professionName'>
+              <h3 className='underline'>CREATIVE</h3><h3 className='highlight'>DEVELOPER</h3>
+              </span>
+          </span>
+        </h1>
+  
+
+      
+
         </div>
 
     );
