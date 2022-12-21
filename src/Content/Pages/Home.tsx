0b6717b1dca36efb07637c71e74260b517af3a14
Stefan Laux <stefan.laux@edu.tbz.ch>
2022-12-21 21:42:46 +0100

    animations added

--- src/Content/Pages/Home.tsx
+++ src/Content/Pages/Home.tsx
@@ -1,53 +1,65 @@
 import * as React from 'react';
 import "../../CSS/Home.css"
-import { useInView } from 'framer-motion';
-import { useEffect } from 'react';
-import { animate } from 'motion';
-
+import {motion, useInView, useScroll, useSpring} from 'framer-motion';
+import {useEffect} from 'react';
+import {animate} from 'motion';
 
 
 export default function Home() {
 
-  const refTitle = React.useRef(null);
-  const isInViewTitle = useInView(refTitle);
+    const refTitle = React.useRef(null);
+    const isInViewTitle = useInView(refTitle);
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 20,
+        restDelta: 0.001,
+        restSpeed: 0.001,
+    });
 
-  
+    useEffect(() => {
+        if (isInViewTitle) {
+            console.log("in view");
+           animate(
+                ".developer",
+                { opacity: 1, marginLeft: "5px" },
+                { delay: .5, duration: 3}
+            )
+            animate(
+                ".creative",
+                { opacity: 1},
+                { delay: 1, duration: 3}
+            )
+        }
 
-useEffect(() => {
-    if (isInViewTitle) {
-      
-    }
-  }, [isInViewTitle]);
-   
+    }, [isInViewTitle]);
+
     return (
-        <div className='wrap' >
+        <div className='wrap'>
+            <div>
+                <motion.div className="scrollProgress" style={{ scaleX }} />
+            </div>
 
-        <div className="header">
+            <div className="header">
 
- <div className="nameTitle">
-            <p>Stefan Laux</p>
-          </div>
-          <div className="nav">
-           
-            <p className='leftBtn'>&#60;Work /&#x3e; </p>
-            <p>|</p>
-            <p className='rightBtn'> &#60;About /&#x3e;</p>
-      
-          </div>
+                <div  className="nameTitle">
+                    <p>Stefan Laux</p>
+                </div>
 
-        </div>
+                <div className="nav">
+                    <a href={"#work"} className='leftBtn'>&#60;Work /&#x3e; </a>
+                    <p>|</p>
+                    <a href={"#about"} className='rightBtn'> &#60;About /&#x3e;</a>
+                </div>
+
+            </div>
 
 
-        <h1 className='m19'>
-          <span className='text-wrapper'>
-            <span className='letters professionName'>
-              <h3 className='underline'>CREATIVE</h3><h3 className='highlight'>DEVELOPER</h3>
-              </span>
-          </span>
-        </h1>
-  
+            <div className='professionName'>
+              <h3 className='underline creative'>CREATIVE</h3><h3 ref={refTitle} className='highlight developer'>DEVELOPER</h3>
+            </div>
 
-      
+
 
         </div>
 
