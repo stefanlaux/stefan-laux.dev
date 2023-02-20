380f039b48054a6efba8023cd3dfb7582f898cdd
StefanLaux <stefan.laux@edu.tbz.ch>
2023-02-20 15:29:21 +0100

    skills

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,5 +1,5 @@
 import './Work.css'
-
+import Grid2 from '@mui/material/Unstable_Grid2';
 
 
 function Work() {
@@ -10,7 +10,39 @@ function Work() {
 
     return (
         <div className="work">
-            <h1>Being created</h1>
+            <h1>SKILLS</h1>
+
+    
+  <Grid2 className={"grid2"} container spacing={2}>
+      <Grid2 item xs={3}>
+        Column 1, Row 1
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 2, Row 1
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 3, Row 1
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 4, Row 1
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 1, Row 2
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 2, Row 2
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 3, Row 2
+      </Grid2>
+      <Grid2 item xs={3}>
+        Column 4, Row 2
+      </Grid2>
+    </Grid2>
+    
+    <div className="blob2"><img src="./images/blob2.png" alt="blob2" /></div>
+    <div className="blob1"><img src="./images/blob1.png" alt="blob1" /></div>
+
         </div>
     )
 }
